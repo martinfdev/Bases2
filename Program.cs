f6f79f5313699a5c9c8a6e99a8278b8695662d5d
Pedro M. <pfrancisco@vivibanco.com.gt>
2024-09-06 14:35:17 -0600

    [UPDATE] Main program

--- Program.cs
+++ Program.cs
@@ -1,44 +1,60 @@
+using AppPdfGenAccountStatus.Data;
+using AppPdfGenAccountStatus.Models;
+using AppPdfGenAccountStatus.Services;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<PdfGenerationService>();
+builder.Services.AddScoped<ISoapService, SoapService>();
+builder.Services.AddScoped<TarjetaService>();
+builder.Services.Configure<SoapServiceSettings>(builder.Configuration.GetSection("SoapServiceSettings"));
+builder.Services.AddDbContext<ApplicationDbContext>(delegate (DbContextOptionsBuilder options)
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+WebApplication app = builder.Build();
+app.UseExceptionHandler(delegate (IApplicationBuilder errorApp)
+{
+    errorApp.Run(async delegate (HttpContext context)
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "text/plain";
+        IExceptionHandlerPathFeature exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        if (exceptionHandlerPathFeature != null)
+        {
+            Exception exception = exceptionHandlerPathFeature.Error;
+            LogException(exception);
+            await context.Response.WriteAsync("Se produjo un error en el servidor.");
+        }
+    });
+});
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(delegate (SwaggerUIOptions c)
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.MapControllers();
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+static void LogException(Exception ex)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", "exceptions.txt");
+    Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
+    string logMessage = $"[{DateTime.Now}] Excepción: {ex.Message}{Environment.NewLine}{ex.StackTrace}{Environment.NewLine}{new string('-', 50)}{Environment.NewLine}";
+    File.AppendAllText(logFilePath, logMessage);
 }
