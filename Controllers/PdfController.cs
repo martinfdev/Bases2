f456df2fb8e81fdb5d980a8f917dc2b22c0a6678
Pedro M. <pfrancisco@vivibanco.com.gt>
2024-09-09 14:28:07 -0600

    [UPDATE] controller get reports with day

--- Controllers/PdfController.cs
+++ Controllers/PdfController.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Threading.Tasks;
 using AppPdfGenAccountStatus.Services;
+using AppPdfRep.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AppPdfGenAccountStatus.Controllers
@@ -17,7 +16,7 @@ namespace AppPdfGenAccountStatus.Controllers
 			_pdfService = pdfService;
 		}
 
-		[HttpGet("generate-reports")]
+		[HttpGet("reporte-mes")]
 		public async Task<IActionResult> GenerateReport([FromQuery] int mes, [FromQuery] int anyo)
 		{
 			if (mes < -1 || mes > 12)
@@ -34,9 +33,35 @@ namespace AppPdfGenAccountStatus.Controllers
 			}
 			catch (Exception ex)
 			{
-				throw new Exception("Error", ex);
+				ExceptionHelper.LogException(ex);
 			}
 			return Ok();
 		}
-	}
+
+        [HttpGet("reporte-fecha")]
+        public async Task<IActionResult> GenerateReportdate([FromQuery] int dia, [FromQuery] int mes, [FromQuery] int anyo)
+        {
+            if(dia < -1 || dia > 30)
+			{
+                return BadRequest("El día debe estar entre 1 y 30.");
+            }
+			if (mes < -1 || mes > 12)
+            {
+                return BadRequest("El mes debe estar entre 1 y 12.");
+            }
+            if (anyo < -1)
+            {
+                return BadRequest("El año debe ser mayor a 1900");
+            }
+            try
+            {
+                await _pdfService.GenerateStatusAccMonth(anyo, mes, dia);
+            }
+            catch (Exception ex)
+            {
+                ExceptionHelper.LogException(ex);
+            }
+            return Ok();
+        }
+    }
 }
