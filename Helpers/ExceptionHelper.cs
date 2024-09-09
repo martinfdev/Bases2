b083841b9193d097ced6350a32c09567b3824514
Pedro M. <pfrancisco@vivibanco.com.gt>
2024-09-09 13:19:15 -0600

    [ADD] Logfor Exceptions

---
+++ Helpers/ExceptionHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace AppPdfRep.Helpers
+{
+    public static class ExceptionHelper
+    {
+        public static void LogException(Exception ex)
+        {
+            string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", "exceptions.txt");
+            Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
+
+            // Detalles de la excepción
+            string logMessage = $@"
+            [{DateTime.Now}] 
+            Tipo de Excepción: {ex.GetType().Name}
+            Mensaje: {ex.Message}
+            Inner Exception: {ex.InnerException?.Message}
+            StackTrace: {ex.StackTrace}
+            ------------------------------------------------";
+
+            // Escribir en archivo de log
+            File.AppendAllText(logFilePath, logMessage);
+        }
+
+
+    }
+}
