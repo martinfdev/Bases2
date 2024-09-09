de6baa4727b27004d5020e63c6bfe267b6dcea31
Pedro M. <pfrancisco@vivibanco.com.gt>
2024-09-09 14:32:26 -0600

    [UPDATE] Refactor code and exception to write file logs

--- Services/PdfGenerationService.cs
+++ Services/PdfGenerationService.cs
@@ -1,5 +1,6 @@
 using AppPdfGenAccountStatus.Helpers;
 using AppPdfGenAccountStatus.Models;
+using AppPdfRep.Helpers;
 using SoapVstec;
 
 namespace AppPdfGenAccountStatus.Services
@@ -88,57 +89,48 @@ namespace AppPdfGenAccountStatus.Services
                 try
                 {
                     string numCardEncripted = EncryptDecrypt_AES.EncryptStringToBytes_Aes(card.NumTarjeta);
-                    if (numCardEncripted != null)
+                    if (numCardEncripted == null)
                     {
-                        Console.WriteLine(numCardEncripted ?? "");
-                        VtcInfoEstCtaTHResponse resultEstCta = await _soapService.GetStatusAccountAsync(dataInput.Month, dataInput.Year, numCardEncripted);
-                        if (resultEstCta != null)
-                        {
-                            //set state account
-                            dataInput.estadoCuenta = resultEstCta;
-                        }
-                        else
-                        {
-                            Console.WriteLine("No se pudo obtener el estado de cuenta");
-                            throw new Exception("No se pudo obtener el estado de cuenta " + dataInput.codeClient);
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("No se pudo encriptar el número de tarjeta");
                         throw new Exception("No se pudo encriptar el número de tarjeta " + dataInput.codeClient);
                     }
 
-                    //get movements account month when day is not 0
+                    // Inicia las tareas en paralelo
+                    var taskEstadoCuenta = _soapService.GetStatusAccountAsync(dataInput.Month, dataInput.Year, numCardEncripted);
+                    Task<VtcObtenerMovimientosCuentaMesResponse> taskMovimientosMes = null;
+                    Task<VtcObtenerMovimientosCuentaFechaResponse> taskMovimientosFecha = null;
+
                     if (day != 0)
                     {
-                        VtcObtenerMovimientosCuentaFechaResponse resultMovDate = await _soapService.GetMovAccountSpecificDayAsync(dataInput.Month, dataInput.Year, dataInput.Day, numCardEncripted);
-                        if (resultMovDate != null)
-                        {
-                            dataInput.movimientosFecha = resultMovDate;
-                        }
+                        taskMovimientosFecha = _soapService.GetMovAccountSpecificDayAsync(dataInput.Month, dataInput.Year, dataInput.Day, numCardEncripted);
                     }
                     else
                     {
-                        VtcObtenerMovimientosCuentaMesResponse resultMovMonth = await _soapService.GetAccountMoveMonth(dataInput.Month, dataInput.Year, numCardEncripted);
-                        if (resultMovMonth != null)
-                        {
-                            dataInput.movimientosMes = resultMovMonth;
-                        }
-                    }
-                    //get movements account status is null 
-                    if(dataInput.estadoCuenta == null)
-                    {
-                        Console.WriteLine("No se pudo obtener el estado de cuenta");
-                        throw new Exception("No se pudo obtener el estado de cuenta " + dataInput.codeClient);
-                    }
-                    //get movements account month is null or movements account date is null
-                    if (dataInput.movimientosMes == null && dataInput.movimientosFecha == null)
-                    {
-                        Console.WriteLine("No se pudo obtener los movimientos de la cuenta");
-                        throw new Exception("No se pudo obtener los movimientos de la cuenta " + dataInput.codeClient);
+                        taskMovimientosMes = _soapService.GetAccountMoveMonth(dataInput.Month, dataInput.Year, numCardEncripted);
                     }
 
+                    // Espera que todas las tareas terminen
+                    await Task.WhenAll(taskEstadoCuenta, taskMovimientosMes ?? Task.CompletedTask, taskMovimientosFecha ?? Task.CompletedTask);
+
+                    // Asigna los resultados de manera condicional
+                    var resultEstCta = await taskEstadoCuenta;
+                    dataInput.estadoCuenta = resultEstCta ?? throw new Exception("No se pudo obtener el estado de cuenta " + dataInput.codeClient);
+
+                    if (day != 0)
+                    {
+                        dataInput.movimientosFecha = await taskMovimientosFecha ?? throw new Exception("No se pudo obtener los movimientos de la fecha " + dataInput.codeClient);
+                    }
+                    else
+                    {
+                        dataInput.movimientosMes = await taskMovimientosMes ?? throw new Exception("No se pudo obtener los movimientos del mes " + dataInput.codeClient);
+                    }
+
+                    // Validar que se obtuvieron correctamente tanto el estado de cuenta como los movimientos
+                    if (dataInput.estadoCuenta == null || (dataInput.movimientosMes == null && dataInput.movimientosFecha == null))
+                    {
+                        throw new Exception("Datos incompletos para la tarjeta " + dataInput.codeClient);
+                    }
+
+                    // Creación del PDF
                     try
                     {
                         PdfHelper createPdf = new PdfHelper(_env);
@@ -146,12 +138,13 @@ namespace AppPdfGenAccountStatus.Services
                     }
                     catch (Exception ex)
                     {
-                        throw new Exception("Error", ex);
+                        throw new Exception("Error al crear el PDF para el cliente " + dataInput.codeClient, ex);
                     }
                 }
                 catch (Exception ex)
                 {
-                    throw new Exception("Error", ex);
+                    ExceptionHelper.LogException(ex);
+                    continue;  // Continúa con la siguiente tarjeta en caso de error
                 }
             }
         }
