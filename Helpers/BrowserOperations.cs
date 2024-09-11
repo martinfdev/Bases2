0ca6dceea553ae95b8d12aabbb624715e97cd738
Pedro M. <pfrancisco@vivibanco.com.gt>
2024-09-11 15:22:30 -0600

    [ADD] openDefaultBrowser when software is init

---
+++ Helpers/BrowserOperations.cs
@@ -0,0 +1,25 @@
+﻿namespace AppPdfRep.Helpers
+{
+    public static class BrowserOperations
+    {
+
+        public static void OpenBrowser(string url)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo { 
+                    FileName = url,
+                    UseShellExecute = true
+
+                });
+            }
+            catch (Exception ex)
+            {
+                ExceptionHelper.LogException(new Exception("Error al abrir el navegador: ", ex));
+
+            }
+        }
+
+
+    }
+}
