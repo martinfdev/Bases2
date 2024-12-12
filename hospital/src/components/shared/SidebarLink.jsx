b6af81705e1ee395cece57beee92597b77b9a44a
Pedro Martin <pedromartinf07@gmail.com>
2024-12-11 19:45:47 -0600

    [ADD] SibarLink global

---
+++ hospital/src/components/shared/SidebarLink.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const SidebarLink = ({ to, icon, label }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 ${
+          isActive ? 'bg-gray-200' : ''
+        }`
+      }
+    >
+      <span className="mr-3">{icon}</span>
+      {label}
+    </NavLink>
+  )
+}
+
+SidebarLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+  label: PropTypes.string.isRequired,
+}
+
+export default SidebarLink
