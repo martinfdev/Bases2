0ddc52ec1e479996a3e4365a633157effa10eea4
Eduardo Reyes <eduardoalex2000@hotmail.com>
2024-12-13 09:06:34 -0600

    [UPDATE] INSERT Administradores
    Se inserto un usuario administrador principal para poder trabajar con el servicio de administracion

--- DB/DML/INSERT_usuarios.sql
+++ DB/DML/INSERT_usuarios.sql
@@ -1,2 +1,5 @@
 -- Usuarios Desarrolladores
-INSERT INTO Usuario (nombres, apellidos, correo, contrasena, id_rol, telefono, dpi, genero, direccion, fecha_ingreso, estado )VALUES('Eduardo Alexander', 'Reyes Gonzalez', 'eduardoalex2000@hotmail.com','Eduardo202010904', 4,'35958027','3013805890101','Masculino','Ciudad de Guatemala','2024/12/13',1);
+INSERT INTO Usuario (nombres, apellidos, correo, contrasena, id_rol, telefono, dpi, genero, direccion, fecha_ingreso, estado )VALUES('Eduardo Alexander', 'Reyes Gonzalez', 'eduardoalex2000@hotmail.com','$2b$12$oTGp1oyyP0cCxlUXVWvjE.1PblEKiz9x.pu4jQVozxBSHr7NrTLKO', 4,'35958027','3013805890101','Masculino','Ciudad de Guatemala','2024/12/13',1);
+
+-- Usuarios Administradores
+INSERT INTO Usuario (nombres, apellidos, correo, contrasena, id_rol, telefono, dpi, genero, direccion, fecha_ingreso, estado )VALUES('Juan', 'Perez', 'juan.perez@example.com','$2b$12$blNuKcn9hueP6xgGuKbNqeOtVdNOGBCEwqFj/APhPhAYmkGnj6Ks2', 3,'55555555','1234567890123','Masculino','Ciudad de Guatemala','2024/12/13',1);
