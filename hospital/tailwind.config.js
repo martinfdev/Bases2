9ddde3434251f4082a7ac8a29abcab01ed895be6
Pedro Martin <pedromartinf07@gmail.com>
2024-12-15 09:44:17 -0600

    [UPATE] config taiwind css

--- hospital/tailwind.config.js
+++ hospital/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 3s ease infinite',
+        bounce: 'bounce 1.5s infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
