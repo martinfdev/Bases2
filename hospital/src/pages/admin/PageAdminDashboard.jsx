30b7220f11a6618d270bdbbedc043bb3ab1a9a07
Pedro Martin <pedromartinf07@gmail.com>
2024-12-16 08:02:56 -0600

    [ADD] dashboard for admin user get data

---
+++ hospital/src/pages/admin/PageAdminDashboard.jsx
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react'
+import {getDashboardData} from '../../services/adminServices'
+
+const  PageAdminDashboard = () => {
+
+    const [dashboardData, setDashboardData] = useState(null)
+   
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const data = await getDashboardData()
+            setDashboardData(data.message)
+        } catch (error) {
+            console.error('Error al obtener datos del dashboard:', error)
+        }}
+        fetchData()
+    }, [])
+
+    return (
+        <div>
+            <h1>{dashboardData}</h1>
+        </div>
+    )
+}
+
+export default PageAdminDashboard;
