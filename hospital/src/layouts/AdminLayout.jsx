6808303e990e07ca6cdf68621b3ee56f3ac20493
Pedro Martin <pedromartinf07@gmail.com>
2024-12-16 07:59:44 -0600

    [ADD] layout for admin

--- hospital/src/layouts/AdminLayout.jsx
+++ hospital/src/layouts/AdminLayout.jsx
@@ -1,69 +1,90 @@
-import {Outlet} from 'react-router-dom'
-import { FaHome, FaUsers, FaUserPlus, FaList, FaChartBar, FaBell, FaListUl } from 'react-icons/fa'
-import Sidebar from '../components/shared/Sidebar'
+import {
+  FaTachometerAlt, FaUserCog, FaUserFriends, FaUserSlash,
+  FaMapMarkedAlt, FaMap, FaUserTag, FaClipboardList,
+  FaFileExport, FaChartPie, FaEnvelopeOpenText, FaInbox
+} from 'react-icons/fa'
+import  Layout  from '../layouts/Layout'
+
 
 const AdminLayout = () => {
+  const menuItems = [
+      {
+          section: null,
+          label: 'Dashboard',
+          path: '/admin/dashboard',
+          icon: <FaTachometerAlt />
+      },
+      {
+          section: 'Gestión de Usuarios',
+          label: 'Crear Usuario',
+          path: '/admin/users/create',
+          icon: <FaUserCog />
+      },
+      {
+          section: 'Gestión de Usuarios',
+          label: 'Listar Usuarios',
+          path: '/admin/users/list',
+          icon: <FaUserFriends />
+      },
+      {
+          section: 'Gestión de Usuarios',
+          label: 'Usuarios Inactivos',
+          path: '/admin/users/inactive',
+          icon: <FaUserSlash />
+      },
+      {
+          section: 'Gestión de Áreas',
+          label: 'Crear Área',
+          path: '/admin/areas/create',
+          icon: <FaMapMarkedAlt />
+      },
+      {
+          section: 'Gestión de Áreas',
+          label: 'Listar Áreas',
+          path: '/admin/areas/list',
+          icon: <FaMap />
+      },
+      {
+          section: 'Gestión de Áreas',
+          label: 'Asignar Pacientes',
+          path: '/admin/areas/assign',
+          icon: <FaUserTag />
+      },
+      {
+          section: 'Bitácora del Sistema',
+          label: 'Visualizar Bitácora',
+          path: '/admin/logs',
+          icon: <FaClipboardList />
+      },
+      {
+          section: 'Reportes',
+          label: 'Generar Reportes (PDF/Excel)',
+          path: '/admin/reports',
+          icon: <FaFileExport />
+      },
+      {
+          section: 'Estadísticas',
+          label: 'Ver Estadísticas',
+          path: '/admin/statistics',
+          icon: <FaChartPie />
+      },
+      {
+          section: 'Notificaciones',
+          label: 'Enviar Notificaciones',
+          path: '/admin/notifications/send',
+          icon: <FaEnvelopeOpenText />
+      },
+      {
+          section: 'Notificaciones',
+          label: 'Solicitudes',
+          path: '/admin/notifications/requests',
+          icon: <FaInbox />
+      },
+  ]
 
-    const menuItems = [
-      {
-        section: null,
-        label: 'Dashboard',
-        path: '/admin/dashboard',
-        icon: <FaHome />,
-      },
-      {
-        section: 'Gestión de Usuarios',
-        label: 'Crear Empleado/Cliente',
-        path: '/admin/users/create',
-        icon: <FaUserPlus />,
-      },
-      {
-        section: 'Gestión de Usuarios',
-        label: 'Listar Usuarios',
-        path: '/admin/users/list',
-        icon: <FaList />,
-      },
-      {
-        section: 'Gestión de Usuarios',
-        label: 'Usuarios Inactivos',
-        path: '/admin/users/inactive',
-        icon: <FaUsers />,
-      },
-      {
-        section: 'Configuraciones de Cuenta',
-        label: 'Modificar Espacio',
-        path: '/admin/users/modify-space',
-        icon: <FaChartBar />,
-      },
-      {
-        section: 'Notificaciones',
-        label: 'Enviar Notificaciones',
-        path: '/admin/notifications',
-        icon: <FaBell />,
-      },
-      {
-        section: 'Notificaciones',
-        label: 'Solicitudes',
-        path: '/admin/requests', 
-        icon: <FaListUl />,
-      }
-    ]
-  
-  
-    return (
-      <div className="flex h-screen">
-  <Sidebar menuItems={menuItems} />
-  <div className="flex-1 flex flex-col">
-    <main className="flex-1 p-6 bg-gray-100 overflow-auto">
-      <Outlet />
-    </main>
-    <footer className="w-full">
-      {/* <Footer /> */}
-    </footer>
-  </div>
-</div>
+  return (
+    <Layout menuItems={menuItems} />
+  )
+}
 
-    )
-  }
-  
-  export default AdminLayout
+export default AdminLayout
