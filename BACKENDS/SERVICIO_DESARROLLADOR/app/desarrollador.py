c406e35caa250c7e177ff471427afd40b7436f04
Eduardo Reyes <eduardoalex2000@hotmail.com>
2024-12-16 10:16:16 -0600

    [ADD]Endpoint registro desarrollador

--- BACKENDS/SERVICIO_DESARROLLADOR/app/desarrollador.py
+++ BACKENDS/SERVICIO_DESARROLLADOR/app/desarrollador.py
@@ -20,5 +20,64 @@ def admin_route(current_user):
     print(current_user)
     return jsonify({"message": f"Welcome desarrollador, user {current_user['id_usuario']}!"}), 200
 
-
 email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+@desarrollador_bp.route('/register', methods=['POST'])
+@token_required
+@desarrollador_required
+def register_user(current_user):
+    data = request.get_json()
+    required_fields = ['nombres','apellidos','correo','contrasena','id_rol','telefono','dpi','genero','direccion','fecha_ingreso','id_especialidad','fecha_vencimiento_colegiado','estado']
+    for field in required_fields:
+        if field not in data:
+            #save_log_param("Insercion", "ERROR", "register", "Desarrollador_Controller", f"Field {field} is required")
+            return jsonify({"error": f"Field {field} is required"}), 400
+    nombres = data['nombres']
+    apellidos = data['apellidos']
+    correo = data['correo']
+    contrasena = data['contrasena']
+    id_rol = data['id_rol'] #maneja diversos tipos de usuarios: doc, enfermera, admin, programador
+    telefono = data['telefono']
+    dpi = data['dpi']
+    genero = data['genero']
+    direccion = data['direccion']
+    fecha_ingreso = data['fecha_ingreso']
+    id_especialidad = data['id_especialidad']
+    fecha_vencimiento_colegiado = data['fecha_vencimiento_colegiado']
+    estado = data['estado']
+    # Validación de formato de email
+    if not re.match(email_regex, correo):
+        #save_log_param("Insercion", "ERROR", "register", "Desarrollador_Controller", "El correo no tiene el formato adecuado")
+        return jsonify({"Error": "El correo no tiene el formato adecuado"}), 400
+    # Encriptar la contraseña
+    hashed_password = bcrypt.hashpw(contrasena.encode('utf-8'), bcrypt.gensalt())
+    #connection db
+    conn = get_db_connection_SQLSERVER()
+    if conn is None:
+        #save_log_param("Insercion", "ERROR", "register", "Desarrollador_Controller", "Error al conectarse con la base de datos")
+        return jsonify({"error": "Error al conectarse con la base de datos"}), 500
+    cursor = conn.cursor()
+    try:
+        print("entro al try")
+        # Verificar si el email o dpi ya exite
+        cursor.execute('SELECT * FROM Usuario WHERE dpi = ? OR correo = ?', (dpi, correo))
+        user_exists = cursor.fetchone()
+        if user_exists:
+            #save_log_param("Insercion", "ERROR", "register", "Desarrollador_Controller", "El correo electronico/dpi ya existe")
+            return jsonify({"Error": "El correo electronico/dpi ya existe"}), 409
+        # Inserción de datos en la tabla Usuarios
+        print(cursor)
+        cursor.execute(''' INSERT INTO Usuario (nombres, apellidos, correo, contrasena, id_rol, telefono, dpi, genero ,direccion, fecha_ingreso, id_especialidad, fecha_vencimiento_colegiado, estado)
+                        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)
+                       ''',(nombres, apellidos, correo, hashed_password.decode('utf-8'), id_rol, telefono, dpi, genero, direccion, fecha_ingreso, id_especialidad, fecha_vencimiento_colegiado, estado))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        #save_log_param("Insercion", "INFO", "register", "Desarrollador_Controller", "Exito, Usuario registrado Correctamente")
+        return jsonify({"message": "Usuario registrado correctamente"}), 201
+    except pyodbc.IntegrityError as e:
+        #save_log_param("Insercion", "ERROR", "register", "Desarrollador_Controller", "Error en la integridad de la base de datos: " + str(e))
+        return jsonify({"Error": "Error en la integridad de la base de datos: " + str(e)}), 400
+    except Exception as e:
+        #save_log_param("Insercion", "ERROR", "register", "Desarrollador_Controller", "Error inesperado")
+        return jsonify({"error": f"Error inesperado: {str(e)}"}), 500
