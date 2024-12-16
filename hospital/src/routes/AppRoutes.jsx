34b67fef1c7b21b633f4382ad9c318ccd8b6f6c1
Pedro Martin <pedromartinf07@gmail.com>
2024-12-16 08:05:26 -0600

    [UPDATE] adding list of route for type user

--- hospital/src/routes/AppRoutes.jsx
+++ hospital/src/routes/AppRoutes.jsx
@@ -2,52 +2,85 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import LoginPage from '../pages/LoginPage'
 import AdminLayout from '../layouts/AdminLayout'
+import PageAdminDashboard from '../pages/admin/PageAdminDashboard'
+import UsersView from '../pages/admin/UserView'
+import DeveloperLayout from '../layouts/DeveloperLayout'
+import DoctorLayout from '../layouts/DoctorLayout'
+import NurseLayout from '../layouts/NurseLayout'
+import NewUserForm from '../components/admin/NewUserForm'
 import NotFound from '../pages/NotFound'
-
+import Unauthorized from '../pages/Unauthorized'
 
 const AppRouter = () => {
-    return (
-      <Routes>
-        {/* routes global for all users */}
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-          
-        {/* Rutas Protegidas para Admin */}
-        { <Route path='admin/' element={ <PrivateRoute roles={[1]}><AdminLayout /></PrivateRoute>}>
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<NotFound />} />
-          <Route path="users/create" element={<NotFound />} />
-          <Route path="users/list" element={<NotFound />} />
-          <Route path="users/inactive" element={<NotFound />} />
-          <Route path="users/modify-space" element={<NotFound />} />
-          <Route path="notifications" element={<NotFound />} />
-          <Route path="requests" element={<NotFound />} />
-          <Route path="*" element={<NotFound />} />
-        </Route> }
-  
-  
-        {/* Rutas Protegidas para Empleados y Clientes */}
-        {/* <Route path='client/' element={<PrivateRoute roles={[2, 3]} children={<ClientLayout />} />}>
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path='dashboard' element={< ClientDashboard />}/>
-          <Route path='files' element={<Files />} />
-          <Route path='shared' element={<SharedWithMe />}/>
-          <Route path='recent' element={<Recent />}/>
-          <Route path='favorites' element={<Favorites />}/> 
-          <Route path='trash' element={<Trash />} /> 
-          <Route path='profile' element={<Profile />} />
-          <Route path='settings' element={<Settings/>} />
-          <Route path='backup' element={<Backup />} />
-          <Route path="*" element={<NotFound />} />
-        </Route> */}
-  
-        {/* Ruta de No Autorizado */}
-        {/* <Route path="/unauthorized" element={<Unauthorized />} /> */}
-  
-        {/* Ruta 404 */}
+
+  return (
+    <Routes>
+      {/* routes global for all users */}
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      {/* protected routes for admin users */}
+      {<Route path='admin/' element={<PrivateRoute roles={[1]}><AdminLayout /></PrivateRoute>}>
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<PageAdminDashboard />} />
+        <Route path="users/create" element={<NewUserForm />} />
+        <Route path="users/list" element={<UsersView />} />
+        <Route path="users/inactive" element={<NotFound />} />
+        <Route path="areas/create" element={<NotFound />} />
+        <Route path="areas/list" element={<NotFound />} />
+        <Route path="areas/assign" element={<NotFound />} />
+        <Route path="logs" element={<NotFound />} />
+        <Route path="reports" element={<NotFound />} />
+        <Route path="statistics" element={<NotFound />} />
+        <Route path="notifications/send" element={<NotFound />} />
+        <Route path="notifications/requests" element={<NotFound />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
-    )
-  }
-  
-  export default AppRouter
+      </Route>}
+
+        {/* protectd routes for developer users */}
+        <Route path='developer/' element={<PrivateRoute roles={[4]}><DeveloperLayout /></PrivateRoute>}>
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<NotFound />} />
+        <Route path="logs" element={<NotFound />} />
+        <Route path="users/create" element={<NotFound />} />
+        <Route path="records/view" element={<NotFound />} />
+        <Route path="records/edit" element={<NotFound />} />
+        <Route path="settings" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+
+      {/* protected routes for doctor users */}
+      <Route path='doctor/' element={<PrivateRoute roles={[2]}><DoctorLayout /></PrivateRoute>}>
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<NotFound />} />
+        <Route path="records/view" element={<NotFound />} />
+        <Route path="records/create" element={<NotFound />} />
+        <Route path="records/edit" element={<NotFound />} />
+        <Route path="areas" element={<NotFound />} />
+        <Route path="areas/assign" element={<NotFound />} />
+        <Route path="notifications" element={<NotFound />} />
+        <Route path="reports/patient" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+
+      {/*protected routes for nurse users */}
+      <Route path='nurse/' element={<PrivateRoute roles={[3]}><NurseLayout /></PrivateRoute>}>
+      <Route index element={<Navigate to="dashboard" replace />} />
+      <Route path="dashboard" element={<NotFound />} />
+      <Route path="patients/list" element={<NotFound />} />
+      <Route path="patients/notes" element={<NotFound />} />
+      <Route path="patients/location" element={<NotFound />} />
+      <Route path="notifications" element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+
+      {/* route for unauthorized users */}
+      <Route path="/unauthorized" element={<Unauthorized />} />
+
+      {/* Route for 404 */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default AppRouter
