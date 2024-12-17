eb51afea3cd64191dab11c1c8c3f56c0b9aa63e0
Pedro Martin <pedromartinf07@gmail.com>
2024-12-16 22:06:39 -0600

    [UPDATE] Title login

--- hospital/src/pages/LoginPage.jsx
+++ hospital/src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ const LoginPage = () => {
             <div className="absolute inset-0 animate-gradient-bg"></div>
             <div className="relative z-10 w-full max-w-md bg-white bg-opacity-90 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg p-6 sm:p-8">
                 <h2 className="text-2xl sm:text-3xl font-semibold text-center text-gray-800 mb-4 sm:mb-6">
-                    Hospital
+                    H - DB2 
                 </h2>
                 <LoginForm />
             </div>
