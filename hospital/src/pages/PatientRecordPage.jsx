87bff5b75b10bb34d5a855363a636d0f3c01a5de
Pedro Martin <pedromartinf07@gmail.com>
2024-12-17 00:21:38 -0600

    [ADD] page for new recor patient

---
+++ hospital/src/pages/PatientRecordPage.jsx
@@ -0,0 +1,12 @@
+import PatientRecordForm from "../components/patientRecord/PatientRecordForm"
+
+const PatientRecordPage = () => {
+    return (
+        <div className="max-w-6xl mx-auto p-6 bg-gray-50 shadow-md rounded-lg">
+            <PatientRecordForm />
+        </div>
+    )
+}
+
+export default PatientRecordPage
+
