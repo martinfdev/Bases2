5a4f7cc877009ab1ea84becc34cb052562f3e16a
Pedro Martin <pedromartinf07@gmail.com>
2024-12-17 20:30:47 -0600

    [ADD] page for partient

---
+++ hospital/src/pages/admin/NewPatientPage.jsx
@@ -0,0 +1,14 @@
+import PatientForm from '../../components/admin/PatientForm'
+
+const NewPatientPage = () => {
+
+    return (
+        <div>
+            <div className="max-w-6xl mx-auto p-6 bg-gray-50 shadow-md rounded-lg">
+                <PatientForm />
+            </div>
+        </div>
+    )
+}
+
+export default NewPatientPage
