1aa68724606894dbb116894a621afacc46636e3b
Pedro Martin <pedromartinf07@gmail.com>
2024-12-19 23:09:56 -0600

    [UPDATE] message error or message

--- hospital/src/components/auth/LoginForm.jsx
+++ hospital/src/components/auth/LoginForm.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth'
+import useAppContext from '../../hooks/useAppContext'
 
 const LoginForm = () => {
   const { login, user } = useAuth()
+  const { addNotification } = useAppContext()
   const navigate = useNavigate()
   const [credentials, setCredentials] = useState({
     identificador: '',
     contrasena: '',
   })
-
   const [errors, setErrors] = useState({})
+  const [error, setError] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const handleChange = (e) => {
@@ -26,7 +28,7 @@ const LoginForm = () => {
       tempErrors.identificador = 'El identificador es requerido.';
     } else if (!/^\d{13}$/.test(credentials.identificador) && !/^\S+@\S+\.\S+$/.test(credentials.identificador)) {
       tempErrors.identificador = 'El identificador debe ser un correo electrónico válido o un número CUI valido 13 dígitos.';
-    }    
+    }
     if (!credentials.contrasena) {
       tempErrors.contrasena = 'La contraseña es requerida.'
     } else if (credentials.contrasena.length < 6) {
@@ -43,8 +45,17 @@ const LoginForm = () => {
       setIsSubmitting(true)
       try {
         await login(credentials)
+        addNotification({
+          type: 'success',
+          message: 'Sesión iniciada correctamente',
+        })
       } catch (error) {
         console.error('Error al iniciar sesión:', error)
+        setError(error.message)
+        addNotification({
+          type: 'error',
+          message: 'Error al iniciar sesión',
+        })
         setErrors({ submit: error.message })
       } finally {
         setIsSubmitting(false)
@@ -62,7 +73,7 @@ const LoginForm = () => {
         navigate('/nurse')
       } else if (user.id_rol === 4) {
         navigate('/developer')
-      }      else {
+      } else {
         navigate('/')
       }
     }
@@ -71,6 +82,7 @@ const LoginForm = () => {
   return (
     <div className=" flex items-center justify-center bg-gray-100 p-4">
       <div className="max-w-md w-full bg-white rounded-lg shadow-md p-6">
+        {error && <p className="text-red-500 mb-4 text-center">{error}</p>}
         <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6">
           Iniciar Sesión
         </h2>
@@ -87,8 +99,8 @@ const LoginForm = () => {
               value={credentials.identificador}
               onChange={handleChange}
               className={`w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 ${errors.identificador
-                  ? 'border-red-500 focus:ring-red-200'
-                  : 'border-gray-300 focus:ring-blue-200'
+                ? 'border-red-500 focus:ring-red-200'
+                : 'border-gray-300 focus:ring-blue-200'
                 }`}
               placeholder="tucorreo@ejemplo.com"
               required
@@ -109,8 +121,8 @@ const LoginForm = () => {
               value={credentials.contrasena}
               onChange={handleChange}
               className={`w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 ${errors.contrasena
-                  ? 'border-red-500 focus:ring-red-200'
-                  : 'border-gray-300 focus:ring-blue-200'
+                ? 'border-red-500 focus:ring-red-200'
+                : 'border-gray-300 focus:ring-blue-200'
                 }`}
               placeholder="********"
               required
