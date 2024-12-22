050a497619dcf063b668609a26d41c22064f7cb9
Pedro Martin <pedromartinf07@gmail.com>
2024-12-22 17:57:26 -0600

    [ADD] page for admin speciality

---
+++ hospital/src/pages/admin/NewSpecialityPage.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import SpecialityForm from '../../components/forms/SpecialityForm'
+import { createSpecialty } from '../../services/adminServices'
+import useAppContext from '../../hooks/useAppContext'
+
+const NewSpecialityPage = () => {
+  const [speciality, setSpeciality] = useState({
+    especialidad: ''
+  })
+  const [error, setError] = useState(null)
+  const { addNotification } = useAppContext()
+
+  const handleChange = (e) => {
+    setSpeciality(prev => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleClick = async () => {
+    try {
+      if (!speciality.especialidad.trim()) {
+        throw new Error('Debe ingresar el nombre de la especialidad')
+      }
+      await createSpecialty(speciality)
+      addNotification({
+        type: 'success',
+        message: 'Especialidad creada correctamente'
+      })
+      // Reseteamos el valor del input
+      setSpeciality({ 
+        especialidad: '' 
+      })
+    } catch (err) {
+      console.error('Error al crear especialidad:', err)
+      setError(err.message)
+      addNotification({
+        type: 'error',
+        message: 'Error al crear especialidad'
+      })
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h1 className='text-center mt-5 text-gray-700 text-2xl font-bold mb-5'>
+        Nueva Especialidad
+      </h1>
+
+      {error && (
+        <p className='text-red-500 text-center mb-5'>
+          {error}
+        </p>
+      )}
+
+      <SpecialityForm 
+        value={speciality} 
+        onChange={handleChange} 
+        onClick={handleClick} 
+      />
+    </div>
+  )
+}
+
+export default NewSpecialityPage
