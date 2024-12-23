2a7386e05dbd304d0af4551a0f88a0d2a6d7759b
Pedro Martin <pedromartinf07@gmail.com>
2024-12-23 08:48:32 -0600

    [ADD] area page for admin

---
+++ hospital/src/pages/admin/NewAreaPage.jsx
@@ -0,0 +1,34 @@
+import AreaForm from '../../components/forms/AreaForm'
+import useAppContext from '../../hooks/useAppContext'
+import { createArea } from '../../services/adminServices'
+
+
+const NewAreaPage = () => {
+    const { addNotification } = useAppContext()
+
+    const handleSubmit = async (data) => {
+        try {
+            await createArea(data)
+            addNotification({
+                type: 'success',
+                message: 'Área creada correctamente',
+            })
+        } catch (error) {
+            console.error('Error al crear el área:', error)
+            addNotification({
+                type: 'error',
+                message: 'Error al crear el área',
+            })
+        }
+    }
+
+    return (
+        <div className="h-full p-4 bg-slate-50 rounded shadow-md">
+            <h1 className="text-2xl font-bold mb-4">Formulario de Área</h1>
+            <AreaForm onSubmit={handleSubmit} />
+        </div>
+    )
+}
+
+export default NewAreaPage
+
