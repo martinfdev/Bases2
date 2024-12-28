578f215a9a70255ccb0dc5c6c43f446c931b636e
Pedro Martin <pedromartinf07@gmail.com>
2024-12-28 15:54:30 -0600

    [UPDATE] edit patient

--- hospital/src/pages/admin/PatientViewPage.jsx
+++ hospital/src/pages/admin/PatientViewPage.jsx
@@ -1,16 +1,16 @@
 
 import { useState, useEffect } from 'react';
 import PatientTable from '../../components/admin/PatientTable'
-import { getPatients, deletePatient, getAreas } from '../../services/adminServices'
+import { getPatients, deletePatient, updatePatient, getAreas } from '../../services/adminServices'
 import useAppContext from '../../hooks/useAppContext'
 import PatientDeleteModal from '../../components/mod/PatientDeleteModal'
-// import PatientEditModal from '../../components/mod/PatientEditModal'
+import PatientEditModal from '../../components/mod/PatientEditModal'
 import PatientViewModal from '../../components/mod/PatientViewModal'
-import { use } from 'react';
+import Waiting from '../../components/shared/Waiting'
 
 const PatientViewPage = () => {
     const [patients, setPatients] = useState([])
-    const [areas, setAreas] = useState([])  
+    const [areas, setAreas] = useState([])
     const [search, setSearch] = useState('')
     const [filteredPatients, setFilteredPatients] = useState([])
     const [error, setError] = useState('')
@@ -19,15 +19,19 @@ const PatientViewPage = () => {
     const [isViewOpen, setIsViewOpen] = useState(false)
     const [selectedPatient, setSelectedPatient] = useState(null)
     const { addNotification } = useAppContext()
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const fetchPatients = async () => {
+            setLoading(true)
             try {
                 const data = await getPatients()
                 setPatients(data.paciente)
             } catch (error) {
                 console.error('Error al obtener datos de pacientes:', error)
                 setError(error.message)
+            }finally{
+                setLoading(false)
             }
         }
         fetchPatients()
@@ -40,7 +44,7 @@ const PatientViewPage = () => {
             setFilteredPatients(patients.filter(p => p.dpi.includes(search)))
         }
     }, [search, patients])
-    
+
     useEffect(() => {
         const fetchAreas = async () => {
             try {
@@ -91,51 +95,79 @@ const PatientViewPage = () => {
         }
     }
 
+    const handleUpdate = async (patient) => {
+        try {
+            const updatedPatient = await updatePatient(patient)
+            const updatedPatients = patients.map(p => p.dpi === updatedPatient.dpi ? updatedPatient : p)
+            setPatients(updatedPatients)
+            setFilteredPatients(updatedPatients)
+            setIsEditOpen(false)
+            addNotification({
+                type: 'success',
+                message: 'Paciente actualizado correctamente',
+            })
+        } catch (error) {
+            addNotification({
+                type: 'error',
+                message: 'Error al actualizar paciente',
+            })
+            console.error('Error al actualizar paciente:', error)
+        }
+    }
+
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Listado de Pacientes</h1>
-            <div className="mb-4 flex items-center space-x-2">
-                <input
-                    type="text"
-                    placeholder="Buscar por DPI..."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    className="border px-3 py-2 rounded focus:outline-none focus:ring focus:border-blue-300"
-                />
-            </div>
-            {error && <p className="text-red-500 mb-4">{error}</p>}
-            <div className="overflow-auto">
-                <PatientTable
-                    patients={filteredPatients}
-                    onDelete={handleDelete}
-                    onEdit={handleEdit}
-                    onView={handleView}
-                />
-            </div>
-            <div className="flex justify-center">
-                <PatientDeleteModal
-                    isOpen={isDeleteOpen}
-                    onClose={() => setIsDeleteOpen(false)}
-                    onConfirm={handleConfirmDelete}
-                    patient={selectedPatient}
-                />
-            </div>
-            {/* <div>
-                <PatientEditModal
-                    isOpen={isEditOpen}
-                    onClose={() => setIsEditOpen(false)}
-                    patient={selectedPatient}
-                />
-            </div> */}
-            <div>
-                <PatientViewModal
-                    isOpen={isViewOpen}
-                    onClose={() => setIsViewOpen(false)}
-                    patient={selectedPatient}
-                    listAreas={areas}
-                />
-            </div>
-        </div>
+        <>
+            {loading ? (
+                <Waiting />
+            ) : (
+                <div className="p-4">
+                    <h1 className="text-2xl font-bold mb-4">Listado de Pacientes</h1>
+                    <div className="mb-4 flex items-center space-x-2">
+                        <input
+                            type="text"
+                            placeholder="Buscar por DPI..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            className="border px-3 py-2 rounded focus:outline-none focus:ring focus:border-blue-300"
+                        />
+                    </div>
+                    {error && <p className="text-red-500 mb-4">{error}</p>}
+                    <div className="overflow-auto">
+                        <PatientTable
+                            patients={filteredPatients}
+                            onDelete={handleDelete}
+                            onEdit={handleEdit}
+                            onView={handleView}
+                        />
+                    </div>
+                    <div className="flex justify-center">
+                        <PatientDeleteModal
+                            isOpen={isDeleteOpen}
+                            onClose={() => setIsDeleteOpen(false)}
+                            onConfirm={handleConfirmDelete}
+                            patient={selectedPatient}
+                        />
+                    </div>
+                    <div>
+                        <PatientEditModal
+                            isOpen={isEditOpen}
+                            onClose={() => setIsEditOpen(false)}
+                            patient={selectedPatient}
+                            listAreas={areas}
+                            onSave={handleUpdate}
+                        />
+                    </div>
+                    <div>
+                        <PatientViewModal
+                            isOpen={isViewOpen}
+                            onClose={() => setIsViewOpen(false)}
+                            patient={selectedPatient}
+                            listAreas={areas}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
