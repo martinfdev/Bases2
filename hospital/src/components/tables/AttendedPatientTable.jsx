13e7bb222ae3e231caa4589e5b3962043b756a1c
Pedro Martin <pedromartinf07@gmail.com>
2024-12-28 18:27:55 -0600

    [ADD] table for patients for area

---
+++ hospital/src/components/tables/AttendedPatientTable.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types'
+
+const AttendedPatientTable = ({ listAttendedPatients}) => {
+    return (
+        <div className="max-w-full mx-auto p-6 bg-white shadow-lg rounded-lg">
+            <table className="min-w-full bg-white rounded-lg overflow-hidden shadow">
+                <thead className='bg-gray-900 text-gray-100'>
+                    <tr>
+                        <th className='py-2 px-4 border-b'>Area</th>
+                        <th className='py-2 px-4 border-b'>Pacientes Atendidos</th>
+                    </tr>
+                </thead>
+                <tbody className='text-gray-700 '>
+                    {listAttendedPatients.map((patient, i) => (
+                        i++,
+                        <tr key={i} className="hover:bg-blue-100">
+                            <td className='py-2 px-4 border-b text-center'>{patient.area}</td>
+                            <td className='py-2 px-4 border-b text-center'>{patient.pacientes_atendidos}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+AttendedPatientTable.propTypes = {
+    listAttendedPatients: PropTypes.arrayOf(PropTypes.shape({
+        area: PropTypes.number.isRequired,
+        pacientes_atendidos: PropTypes.string.isRequired
+    })).isRequired
+}
+
+export default AttendedPatientTable
