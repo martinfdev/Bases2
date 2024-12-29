9f2ba3b01ba6e4f002d4c9ac081db62a3de04eea
Pedro Martin <pedromartinf07@gmail.com>
2024-12-28 19:23:00 -0600

    [UPDATE] remove not usable item

--- hospital/src/layouts/Layout.jsx
+++ hospital/src/layouts/Layout.jsx
@@ -8,9 +8,7 @@ const Layout = ({menuItems}) => {
     <div className="flex h-screen">
       <Sidebar menuItems={menuItems} />
       <div className="flex-1 flex flex-col">
-       
           <Header />
-       
         <main className="flex-1 p-6 bg-gray-100 overflow-auto">
           <Outlet />
         </main>
