ce801cd634bea010c2010fd7e3978482a44fd36b
Pedro Martin <pedromartinf07@gmail.com>
2024-12-29 21:03:41 -0600

    [UPDATE] unsigne area

--- hospital/src/components/admin/PatientTable.jsx
+++ hospital/src/components/admin/PatientTable.jsx
@@ -1,6 +1,6 @@
 import PropsTypes from 'prop-types'
 
-const PatientTable = ({ patients, onDelete, onEdit, onView }) => {
+const PatientTable = ({ patients, onDelete, onEdit, onView, buttonStatus }) => {
     const getState = (state) => {
         return state === "1" ? "Activo" : "Inactivo"
     }
@@ -12,7 +12,6 @@ const PatientTable = ({ patients, onDelete, onEdit, onView }) => {
 
     return (
         <div className="max-w-full mx-auto p-6 bg-white shadow-lg rounded-lg">
-            <h2 className="text-2xl font-semibold mb-6">Lista de Pacientes</h2>
             <table className="min-w-full bg-white rounded-lg overflow-hidden shadow">
                 <thead className="bg-gray-900 text-gray-100 ">
                     <tr>
@@ -40,12 +39,15 @@ const PatientTable = ({ patients, onDelete, onEdit, onView }) => {
                             <td className="py-2 px-4 border-b text-center">{formatDate(patient.fecha_nacimiento)}</td>
                             <td className="py-2 px-4 border-b">{patient.telefono}</td>
                             <td className="py-2 px-4 border-b">{patient.direccion}</td>
-                            <td className="py-2 px-4 border-b text-center">{patient.id_area}</td>
+                            <td className="py-2 px-4 border-b text-center">{patient.id_area?? "N/A"}</td>
                             <td className="py-2 px-4 border-b text-center">{getState(patient.estado)}</td>
                             <td className="py-3 px-4 flex space-x-2">
                                 <button
                                     onClick={() => onView(patient)}
-                                    className="flex items-center bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition"
+                                    disabled={buttonStatus}
+                                    className={buttonStatus?"flex items-center bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition cursor-not-allowed "
+                                        :"flex items-center bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 transition"
+                                    }
                                 >
                                     <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -55,7 +57,9 @@ const PatientTable = ({ patients, onDelete, onEdit, onView }) => {
                                 </button>
                                 <button
                                     onClick={() => onEdit(patient)}
-                                    className="flex items-center bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition"
+                                    disabled={buttonStatus}
+                                    className= {buttonStatus?"flex items-center bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition cursor-not-allowed "
+                                        :"flex items-center bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition"}  
                                 >
                                     <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 113.828 3.828L11.828 15H9v-2.828l8.586-8.586z" />
@@ -64,7 +68,10 @@ const PatientTable = ({ patients, onDelete, onEdit, onView }) => {
                                 </button>
                                 <button
                                     onClick={() => onDelete(patient)}
-                                    className="flex items-center bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition"
+                                    disabled={buttonStatus}
+                                    className={buttonStatus? "flex items-center bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition cursor-not-allowed "
+                                        :"flex items-center bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 transition"
+                                    }
                                 >
                                     <svg className="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -84,7 +91,8 @@ PatientTable.propTypes = {
     patients: PropsTypes.array.isRequired,
     onDelete: PropsTypes.func.isRequired,
     onEdit: PropsTypes.func.isRequired,
-    onView: PropsTypes.func.isRequired
+    onView: PropsTypes.func.isRequired,
+    buttonStatus: PropsTypes.bool.isRequired
 }
 
 export default PatientTable
