fc1ec3b813156b84e3864b301a0152ceef327bcb
Pedro Martin <pedromartinf07@gmail.com>
2024-12-30 23:54:15 -0600

    [ADD] dev create user

---
+++ hospital/src/pages/dev/NewUserRegisterDevPage.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+import PropsTypes from 'prop-types'
+import { getSpecialties, registerUser } from '../../services/developerService'
+import NewUserForm from '../../components/forms/NewUserForm'
+import Waiting from '../../components/shared/Waiting'
+import useAppContext from '../../hooks/useAppContext'
+
+const NewUserRegisterDevPage = ({ currentUserRole }) => {
+    const [specialities, setSpecialities] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const { addNotification } = useAppContext()
+
+    useEffect(() => {
+        const fetchSpecialities = async () => {
+            try {
+                const response = await getSpecialties()
+                setSpecialities(response.especialidades)
+                setLoading(false)
+            } catch (error) {
+                console.log(error)
+                setError('Ocurrió un error al intentar obtener los datos')
+            }
+        }
+        fetchSpecialities()
+    }, [])
+
+    const handleRegisterUser = async (user) => {
+        setLoading(true)
+        try {
+            await registerUser(user)
+            addNotification({
+                type: 'success',
+                message: 'Usuario registrado correctamente'
+            })
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            addNotification('Ocurrió un error al intentar registrar el usuario', 'error')
+        }
+    }
+
+    return (
+        <>
+            {loading ? (
+                <Waiting />
+            ) : (
+                <div>
+                    {error && <p className="text-red-500">{error}</p>}
+                    <NewUserForm
+                        currentUserRole={currentUserRole}
+                        listSpecialities={specialities}
+                        registerUser={handleRegisterUser}
+                    />
+                </div>
+            )}
+        </>
+    )
+}
+
+NewUserRegisterDevPage.propTypes = {
+    currentUserRole: PropsTypes.number.isRequired
+}
+
+export default NewUserRegisterDevPage
