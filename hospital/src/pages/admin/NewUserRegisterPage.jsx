34ec2f5f572ef814d5bcd1ca5d558b42d641406f
Pedro Martin <pedromartinf07@gmail.com>
2024-12-30 22:50:43 -0600

    [UPDATE] method send

---
+++ hospital/src/pages/admin/NewUserRegisterPage.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import PropsTypes from 'prop-types'
+import { getSpecialties, registerUser } from '../../services/adminServices'
+import NewUserForm from '../../components/forms/NewUserForm'
+import Waiting from '../../components/shared/Waiting'
+import useAppContext  from '../../hooks/useAppContext'
+
+const NewUserRegisterPage = ({currentUserRole}) => {
+    const [specialities, setSpecialities] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const {addNotification} = useAppContext()
+
+    useEffect(() => {
+        const fetchSpecialities = async () => {
+            try {
+                const response = await getSpecialties()
+                setSpecialities(response.especialidades)
+                setLoading(false)
+            } catch (error) {
+                console.log(error)
+                setError('Ocurrió un error al intentar obtener los datos')
+            }
+        }
+        fetchSpecialities()
+    }, [])
+
+    const handleRegisterUser = async (user) => {
+        try {
+            await registerUser(user)
+            addNotification({
+                type: 'success',
+                message: 'Usuario registrado correctamente'
+        })
+        } catch (error) {
+            console.log(error)
+            addNotification('Ocurrió un error al intentar registrar el usuario', 'error')
+        }
+    }
+
+    return (
+        <>
+            {loading && <Waiting />}
+            {error && <p className="text-red-500">{error}</p>}
+            <NewUserForm currentUserRole={currentUserRole} lsitSpecialities={specialities} registerUser={handleRegisterUser}  />
+        </>
+    )
+}
+
+NewUserRegisterPage.propTypes = {
+    currentUserRole: PropsTypes.number.isRequired
+}
+
+export default NewUserRegisterPage
